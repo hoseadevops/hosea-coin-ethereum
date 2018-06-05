504385fe175504c15aa7ca6c7f79d4e1e11fdb02
hosea <starit@msn.cn>
2018-06-06 06:25:47 +0800

    fiexed

--- contracts/HoseaCoin.sol
+++ contracts/HoseaCoin.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.21;
 
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
+
 contract HoseaCoin is StandardToken {
 
-  string public name = "HoseaCoin";
+    string public name = "HoseaCoin";
 
-  string public symbol = "HSC";
-  // 小数点后18个0
-  uint public decimals = 18;
-  // 100万
-  uint public INITIAL_SUPPLY = 1000000 * (10 ** decimals);
+    string public symbol = "HSC";
+    // 小数点后18个0
+    uint public decimals = 18;
 
-  function HamburgerCoin() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-  }
+    function HoseaCoin() public {
+        // 100万
+        totalSupply_ = 1000000 * (10 ** decimals);
+        balances[msg.sender] = totalSupply_;
+    }
 }
